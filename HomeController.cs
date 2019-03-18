81263076b0e76f1fdde93bfc95ddc9e1d1acecf9
Vinay.KV <Vinay.KV@IN8INE0479>
2019-03-18 13:35:39 +0530

    Review2 changes

--- HomeController.cs
+++ HomeController.cs
@@ -16,7 +16,7 @@ namespace BundleAndMinification.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+                 ViewBag.Message = "Your application description page.";
             return View();
         }
 
